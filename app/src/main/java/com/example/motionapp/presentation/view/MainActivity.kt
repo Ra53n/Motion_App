9107f8ce84fdb365cc059b6bf27368934d7fcdc2
Ra53n <nikkon2001@mail.ru>
2023-02-04 11:59:42 +0500

    Добавил бегущего человека с анимацией. Добавил простое окружение для человека. Почистил проект

---
+++ app/src/main/java/com/example/motionapp/presentation/view/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.example.motionapp.presentation.view
+
+import androidx.appcompat.app.AppCompatActivity
+import com.example.motionapp.R
+
+class MainActivity : AppCompatActivity(R.layout.main_activity) {
+}
